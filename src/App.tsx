9030dd21a2b2d398ce30688812c102ddd4526e27
ArtiomOganesyan <artiomoganesyan@gmail.com>
2023-01-13 18:42:10 +0500

    css fix table position

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,22 @@ import "./app.css";
 import Table from "./components/Table/Table";
 
 const App = () => {
-  return (
-    <div>
-      <Table></Table>
-    </div>
-  );
+  const [teachers, setTeachers] = createSignal([]);
+  const [groups, setGroups] = createSignal([]);
+
+  const handle = () => {
+    console.log("asdfasdfasdf");
+    setTeachers(["asd"]);
+    setGroups(["adsf"]);
+  };
+
+  createEffect(() => {
+    console.log("ddd");
+    console.log(teachers());
+    console.log(groups());
+  });
+
+  return <div>{0 ? <div onClick={handle}>test</div> : <Table></Table>}</div>;
 };
 
 export default App;
